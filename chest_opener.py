bf597fcda7958e75f0de1e6c8abfa01ad03fbc4c
faizanSarfrazNovade <faizansarfraz95@hotmail.com>
2023-09-23 00:56:48 +0200

    initial commit

---
+++ chest_opener.py
@@ -0,0 +1,18 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import numpy as np
+import random
+import win32api,win32con
+
+time.sleep(2)
+
+def click(x,  y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    time.sleep(0.1)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+
+pyautogui.displayMousePosition()
+print("test")
